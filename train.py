920814f4f6a7337d0bf285dc7926d5210e10aa2c
MirzaolimTuhtasinov <mirzaolimtoxtasinov8@gmail.com>
2026-09-22 06:24:11 +0900

    feat: MIT TASK M

--- train.py
+++ train.py
@@ -1,29 +1,41 @@
-# MIT TASK L
-def reverseSentence(sentence):
+# MIT TASK M
+def getSquareNumbers(numbers):
     new_array = []
-    new_input = sentence.split(" ")
-    for ele in new_input:
-        ele2 = reversed(ele)
-        final_ele = "".join(ele2)
-        new_array.append(final_ele)
-
-    return " ".join(new_array)
+    for ele in numbers:
+        new_array.append({f"number: {ele}, square: {ele * ele}"})
+    return new_array
 
 
-result = reverseSentence('we like python!')
-print("Result:", result)
+array1 = getSquareNumbers([3, 4, 6, 7, 9])
+print("Array1:", array1)
 
 
-def reverseSentence2(input):
-    new_array = []
-    new_input = input.split(" ")
+# # MIT TASK L
+# def reverseSentence(sentence):
+#     new_array = []
+#     new_input = sentence.split(" ")
+#     for ele in new_input:
+#         ele2 = reversed(ele)
+#         final_ele = "".join(ele2)
+#         new_array.append(final_ele)
 
-    for ele in new_input:
-        final_ele = "".join(reversed(ele))
-        new_array.append(final_ele)
-
-    return " ".join(new_array)
+#     return " ".join(new_array)
 
 
-result2 = reverseSentence2("we like coding!")
-print("Result2:", result2)
+# result = reverseSentence('we like python!')
+# print("Result:", result)
+
+
+# def reverseSentence2(input):
+#     new_array = []
+#     new_input = input.split(" ")
+
+#     for ele in new_input:
+#         final_ele = "".join(reversed(ele))
+#         new_array.append(final_ele)
+
+#     return " ".join(new_array)
+
+
+# result2 = reverseSentence2("we like coding!")
+# print("Result2:", result2)
