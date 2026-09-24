501abf7bacdcd03839956e0aa72d33f1270a7c37
MirzaolimTuhtasinov <mirzaolimtoxtasinov8@gmail.com>
2026-09-24 21:49:50 +0900

    fix: modify routing system and controllers

---
+++ src/controllers/restaurant.controller.ts
@@ -0,0 +1,29 @@
+
+import { Request, Response } from 'express';
+import {T} from '../libs/types/common';
+
+const restaurantController: T = {};
+restaurantController.goHome = (req: Request, res: Response) => {
+    try {
+        res.send('Home Page');
+    } catch(err) {
+        console.log("ERROR, goHome:", err)
+    }
+};
+restaurantController.getLogin = (req: Request, res: Response) => {
+    try {
+        res.send('Login Page');
+    } catch(err) {
+        console.log("ERROR, getLogin:", err)
+    }
+};
+restaurantController.getSignup = (req: Request, res: Response) => {
+    try {
+        res.send('Sign Up Page');
+    } catch(err) {
+        console.log("ERROR, getSignUp:", err)
+    }
+};
+
+
+export default restaurantController;
